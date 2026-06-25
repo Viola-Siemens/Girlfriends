552512d7163f4f04622ba107d92fb8c3131d6764
liudongyu <10185101162@stu.ecnu.edu.cn>
2026-06-25 17:10:39 +0800

    feat(REQ-7): 配置文件

---
+++ src/main/java/com/hexagram2021/girlfriends/common/config/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 配置文件喵~
+ *
+ * @author liudongyu
+ */
+@NullMarked
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.hexagram2021.girlfriends.common.config;
+
+import com.mojang.logging.annotations.FieldsAreNonnullByDefault;
+import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
+import org.jspecify.annotations.NullMarked;
+
+import javax.annotation.ParametersAreNonnullByDefault;
