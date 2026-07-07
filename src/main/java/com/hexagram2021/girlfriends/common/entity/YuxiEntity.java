ab5124b368f5d46462f8c9daf9600856e96bdf27
liudongyu <10185101162@stu.ecnu.edu.cn>
2026-07-07 18:16:27 +0800

    refactor(REQ-7): 简化类结构和继承关系

--- src/main/java/com/hexagram2021/girlfriends/common/entity/YuxiEntity.java
+++ src/main/java/com/hexagram2021/girlfriends/common/entity/YuxiEntity.java
@@ -1,6 +1,7 @@
 package com.hexagram2021.girlfriends.common.entity;
 
 import com.google.common.collect.ImmutableList;
+import com.hexagram2021.girlfriends.common.character.GirlfriendType;
 import com.hexagram2021.girlfriends.common.character.GirlfriendTypes;
 import com.hexagram2021.girlfriends.common.entity.ai.GirlfriendsActivities;
 import com.hexagram2021.girlfriends.common.entity.ai.GirlfriendsEnvironmentAttributes;
@@ -9,6 +10,7 @@ import com.hexagram2021.girlfriends.common.entity.ai.GirlfriendsSensorTypes;
 import com.hexagram2021.girlfriends.common.entity.ai.behavior.*;
 import com.hexagram2021.girlfriends.common.item.GirlfriendsItemTags;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -46,6 +48,11 @@ public class YuxiEntity extends GirlfriendEntity {
 	@Override
 	public Identifier getGirlfriendTypeId() {
 		return GirlfriendTypes.YUXI_ID;
+	}
+
+	@Override
+	public Holder<GirlfriendType> getGirlfriendType() {
+		return GirlfriendTypes.YUXI;
 	}
 
 	@Override
